264c7523dcd1ed49384005a22ee40098c3429ac6
MigrationTool <emarian323@gmail.com>
2022-05-20 06:23:02 +0300

    Add new customer 2720 and company 29927

---
+++ vbnxt-customer2720.tf
@@ -0,0 +1,18 @@
+
+azure_customer_number = 2720
+customer_id = 8792458
+company_number = 29927
+environment = "dev"
+data_center = 'ne'
+
+databases = {
+
+  F0004 = {
+    database_name = "F0004"
+    db_storage_bacpac = "https://emidbmigrationnesa.blob.core.windows.net/customer-db-backup/emi-F0004_2021-08-16T22-01Z.bacpac"
+  }
+
+  # @DB-placeholder
+
+  # don't remove placeholder as it's used when adding clients to existing customer
+}
